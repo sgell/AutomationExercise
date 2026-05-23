67ada1f9dbe6f867de99c1d869d323e50106b22d
sgell <zamund4e@gmail.com>
2026-05-23 20:42:41 +0300

    fix LoginPage exports

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,34 +1,36 @@
-class LoginPage{
-    constructor(page){
-        this.page = page;
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+  }
 
-        this.passwordInput = page.locator('[name="password"]');
-        this.loginButton = page.locator('[data-qa="login-button"]');
-    }
-    async navigate(){
-        await this.page.goto("/login");
-    }
-    async fillEmail(email){
-        await this.page.locator('[data-qa="login-email"]').fill(email);
-    }
-    async fillPassword(password){
-      await this.page.locator('[name="password"]').fill(password)
-    }
-    async clickLoginButton() {
-        await this.page.locator('[data-qa="login-button"]').click();
-    }
-    async login(email,password){
-        await this.fillEmail(email);
-        await this.fillPassword(password);
-        await this.clickLoginButton();
-    }
+  async navigate() {
+    await this.page.goto('/login');
+  }
 
-    async acceptCookies() {
-        const consent = this.page.locator('button:has-text("Consent")');
-        if (await consent.isVisible()) {
-            await consent.click();
-    }
+  async fillEmail(email) {
+    await this.page.locator('[data-qa="login-email"]').fill(email);
+  }
 
+  async fillPassword(password) {
+    await this.page.locator('[name="password"]').fill(password);
+  }
+
+  async clickLoginButton() {
+    await this.page.locator('[data-qa="login-button"]').click();
+  }
+
+  async login(email, password) {
+    await this.fillEmail(email);
+    await this.fillPassword(password);
+    await this.clickLoginButton();
+  }
+
+  async acceptCookies() {
+    const consent = this.page.locator('button:has-text("Consent")');
+    if (await consent.isVisible()) {
+      await consent.click();
+    }
+  }
 }
-module.exports = {LoginPage};
 
+module.exports = { LoginPage };
